3173f250870850f28956473d605a467dc1988542
Caroline Mimeault <carolinemimeault@gmail.com>
2026-02-14 18:57:18 -0500

    initial commit with minimal working code

---
+++ rsbuild.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from '@rsbuild/core';
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+    html: {
+        template: './static/index.html',
+    },
+});
